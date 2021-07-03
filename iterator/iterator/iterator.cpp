57ff185cb70d6e822db41000b46159999ca96c6d
Евгений <rabota142@inbox.ru>
2021-07-03 20:31:37 +0300

    Added Iterator

---
+++ iterator/iterator/iterator.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include "vector.h"
+
+
+int main() {
+	vector<int> a{ 1,2,3,4 };
+	auto start = a.begin();
+	while (start != a.end()) {
+		std::cout << *start << " " << std::endl;
+		start++;
+	}
+	std::cout <<"Iterator + n = " << a.begin() + 2 << std::endl;
+	std::cout <<"Equality = " << (a.begin() == a.begin()) << std::endl;
+}
---
+++ iterator/iterator/vector.h
@@ -0,0 +1,133 @@
+#ifndef true
+#include <iostream>
+#endif
+
+template <typename T>
+class vector
+{
+public:
+    class Iterator{
+    public:
+        Iterator(T* first) : element(first) {}
+        T& operator+ (int n) { return *(element + n); }
+        T& operator- (int n) { return *(element - n); }
+        T& operator++ (int) { return *element++; }
+        T& operator-- (int) { return *element--; }
+        T& operator++ () { return *++element; }
+        T& operator-- () { return *--element; }
+        bool operator!= (const Iterator& it) { return element != it.element; }
+        bool operator== (const Iterator& it) { return element == it.element; }
+        T& operator* () { return *element; }
+    private:
+        T* element;
+    };
+    Iterator begin() { return my_data; };
+    Iterator end() { return (my_data + my_size); }
+    vector() : my_data(nullptr), my_size(0), my_capacity(0) {};
+    vector(int const size, T const elem) : my_size(size), my_capacity(size * 2)
+        , my_data(static_cast<T*>(::operator new(sizeof(T)* my_capacity))) {
+        for (int i = 0; i < size; ++i) {
+            new (my_data + i) T(elem);
+        }
+    }
+    vector(const vector& other) : my_size(other.my_size), my_capacity(other.my_capacity)
+        , my_data(static_cast<T*>(::operator new(other.my_capacity))) {
+        for (int i = 0; i < my_size; ++i) {
+            new (my_data + i) T(other.my_data[i]);
+        }
+    }
+    vector(std::initializer_list<T> list_elem) : my_size(0), my_capacity(list_elem.size() * 2)
+        , my_data(static_cast<T*>(::operator new(sizeof(T)* list_elem.size() * 2))) {
+        for (auto& elem : list_elem) {
+            new (my_data + my_size) T(elem);
+            ++my_size;
+        }
+    }
+    vector(vector&& vec) : my_size(vec.my_size), my_capacity(vec.my_capacity), my_data(vec.my_data) {
+        vec.my_data = nullptr;
+    }
+    vector& operator=(vector&& vec) {
+        if (this != &vec) {
+            operator delete(my_data);
+            my_size = vec.my_size;
+            my_capacity = vec.my_capacity;
+            my_data = vec.my_data;
+            vec.my_data = nullptr;
+            vec.my_capacity = 0;
+            vec.my_size = 0;
+        }
+        return *this;
+    }
+    vector& operator=(const vector& vec) {
+        if (this != &vec) {
+            operator delete(my_data);
+            my_size = vec.my_size;
+            my_capacity = vec.my_capacity;
+            my_data = static_cast<T*>(operator new(my_capacity));
+            for (int i = 0; i < my_size; ++i) {
+                new (my_data + i) T(vec.my_data[i]);
+            }
+        }
+        return *this;
+    }
+    void pop_back() {
+        my_data[my_size].~T();
+        --my_size;
+    }
+    void resize(unsigned int const size) {
+        if (size < my_size) {
+            for (int i = my_size; i > size; --i)
+                my_data[i].~T();
+        }
+        else if (size > my_size) {
+            for (int i = my_size; i < size; ++i)
+                this->push_back(0);
+        }
+        my_size = size;
+    }
+    void push_back(T const elem) {
+        if (my_size < my_capacity) {
+            new (my_data + my_size) T(elem);
+        }
+        else {
+            reserve(my_capacity * 2);
+            new (my_data + my_size) T(elem);
+        }
+        my_size++;
+    }
+    unsigned int capacity() const { return my_capacity; }
+    unsigned int size() const { return my_size; }
+    T& operator[] (unsigned int index) const {
+        return my_data[index];
+    }
+    T& at(unsigned int const index) const {
+        if (index >= my_size) throw std::runtime_error("Index out of range");
+        else return my_data[index];
+    }
+    ~vector() noexcept {
+        clear();
+    };
+    void reserve(unsigned int const n) noexcept {
+        if (n > my_capacity) {
+            my_capacity = n;
+            T* tmp = static_cast<T*>(::operator new(sizeof(T) * my_capacity));
+            for (int i = 0; i < my_size; ++i) {
+                new (tmp + i) T(my_data[i]);
+            }
+            operator delete(my_data);
+            my_data = tmp;
+        }
+    }
+    void clear() noexcept {
+        for (int i = 0; i < my_size; ++i)
+            my_data[i].~T();
+        operator delete (my_data);
+        my_data = nullptr;
+        my_size = 0;
+        my_capacity = 0;
+    }
+private:
+    unsigned int my_capacity;
+    unsigned int my_size;
+    T* my_data;
+};
