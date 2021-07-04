bfe8f8041b9709aba0e9b3a48d69334fc04009e3
Евгений <rabota142@inbox.ru>
2021-07-04 12:02:06 +0300

    Added erase

--- iterator/iterator/iterator.cpp
+++ iterator/iterator/iterator.cpp
@@ -2,17 +2,21 @@
 #include "vector.h"
 
 
-int main() {
-	vector<int> a{ 1,2,3,4 };
-	a.push_back(10);
+template <typename T>
+void printVector(vector<T>& a) {
+	std::cout << "Size = " << a.size() << std::endl;
 	auto start = a.begin();
 	while (start != a.end()) {
 		std::cout << (*start) << " " << std::endl;
-		start++;
+		++start;
 	}
-	std::cout <<"Iterator + n = " << *(a.begin() + 2) << std::endl;
-	std::cout <<"Equality = " << (a.begin() == a.begin()) << std::endl;
-	auto test = a.end();
-	advance(test, -4);
-	std::cout << "Advance = " << *test << std::endl;
+}
+
+
+int main() {
+	vector<int> a{ 1,2,3,4 };
+	a.push_back(10);
+	printVector(a);
+	a.erase(a.begin());
+	printVector(a);
 }
--- iterator/iterator/vector.h
+++ iterator/iterator/vector.h
@@ -148,6 +148,15 @@ public:
             my_data = tmp;
         }
     }
+    //I'm not sure
+    void erase(iterator it) {
+        if (it >= cend()) { throw std::runtime_error("Iterator out of range"); }
+        while (it != (cend() - 1)) {
+            *it = *(it + 1);
+            ++it;
+        }
+        pop_back();
+    }
 
 public:
     unsigned int capacity() const { return my_capacity; }
@@ -183,7 +192,7 @@ void advance_impl(T& it, typename my_iterator<T>::difference_type n, std::input_
     }
 }
 
-//++ --
+// ++ --
 template <typename T>
 void advance_impl(T& it, typename my_iterator<T>::difference_type n, std::bidirectional_iterator_tag) {
     while (n > 0) {
